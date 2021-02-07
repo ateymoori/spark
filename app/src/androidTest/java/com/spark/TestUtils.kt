37f86f7b98bbe2df18bba1b44c5a2290ccbef9ec
Amirhossein <amir@mobilityonegroup.com>
2021-02-07 11:17:31 +0800

    APIs have version, now.

--- app/src/androidTest/java/com/spark/TestUtils.kt
+++ app/src/androidTest/java/com/spark/TestUtils.kt
@@ -8,6 +8,17 @@ import java.io.InputStreamReader
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import androidx.core.util.Preconditions
+import androidx.fragment.app.Fragment
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+
+import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.testing.FragmentScenario
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class FileReader(path: String) {
     val content: String
@@ -41,3 +52,40 @@ fun <T> LiveData<T>.getOrAwaitValueAndroidTest(
     @Suppress("UNCHECKED_CAST")
     return data as T
 }
+
+
+
+
+@ExperimentalCoroutinesApi
+inline fun  <reified T: Fragment> launchFragmentInHiltContainer(
+    fragmentArgs: Bundle? = null,
+    themeResId: Int = R.style.FragmentScenarioEmptyFragmentActivityTheme,
+    fragmentFactory: FragmentFactory? = null,
+    crossinline action: T.() -> Unit = {}
+) {
+    val mainActivityIntent = Intent.makeMainActivity(
+        ComponentName(
+            ApplicationProvider.getApplicationContext(),
+            HiltTestActivity::class.java
+        )
+    ).putExtra(FragmentScenario.EmptyFragmentActivity.THEME_EXTRAS_BUNDLE_KEY, themeResId)
+
+    ActivityScenario.launch<HiltTestActivity>(mainActivityIntent).onActivity { activity->
+        fragmentFactory?.let {
+            activity.supportFragmentManager.fragmentFactory = it
+        }
+
+        val fragment = activity.supportFragmentManager.fragmentFactory.instantiate(
+            androidx.test.espresso.core.internal.deps.guava.base.Preconditions.checkNotNull(T::class.java.classLoader),
+            T::class.java.name
+        )
+
+        fragment.arguments = fragmentArgs
+
+        activity.supportFragmentManager.beginTransaction().add(android.R.id.content, fragment, "").commitNow()
+
+        (fragment as T).action()
+    }
+}
+
+
